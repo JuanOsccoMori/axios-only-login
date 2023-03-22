9ec366e46fa7e73d337877c007a44e419635602c
Juan Oscco <juan.osccomori@gmail.com>
2023-03-22 15:20:34 -0500

    subiendo la primera version de una peticion post en login

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+### `create .env`
+REACT_APP_API_URL
+
 ### `npm start`
 
 Runs the app in the development mode.\
--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import axios from "axios"
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [responseData, setResponseData] = useState({});
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.post(process.env.REACT_APP_API_URL, {
+      username,
+      password,
+    })
+      .then(response => {
+        console.log(response.data);
+        localStorage.setItem('token', response.data.token);
+        setResponseData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    setResponseData({})
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setResponseData({ token });
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Ingresar</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Usuario:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Contraseña:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <button type="submit">Iniciar sesión</button>
+        </form>
+        {responseData.token && (
+          <div>
+            <p>Nombre de la empresa: {responseData.persona.nombres}</p>
+            <p>Direccion de la empresa: {responseData.persona.direccion}</p>
+            <button onClick={handleLogout}>x</button>
+          </div>
+        )}
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
