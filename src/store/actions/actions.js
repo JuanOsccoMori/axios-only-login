73530fa761ebbee6eb3865bf62781e4125c11d01
Juan Oscco <juan.osccomori@gmail.com>
2023-03-23 07:40:30 -0500

    agregando las variables de accion y los payload en el archivo actions.js

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,16 +1,29 @@
 import axios from 'axios';
 
+// Variables de accion
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+
+const requestLogin = response => ({
+    type:LOGIN_REQUEST,
+    payload: response.data
+})
 
 const successLogin = response => ({
     type: LOGIN_SUCCESS,
     payload: response.data
 })
 
+const failureLogin = response => ({
+    type: LOGIN_FAILURE,
+    payload: response.error.message
+})
+
 export const axiosLogin = (username, password) => async (dispatch) => {
     try {
+        dispatch(requestLogin)
         const response = await axios.post(process.env.REACT_APP_API_URL,
             { username, password }
         )
@@ -21,6 +34,6 @@ export const axiosLogin = (username, password) => async (dispatch) => {
 
     }
     catch (error) {
-        console.log(error)
+        dispatch(failureLogin(error))
     }
 }
